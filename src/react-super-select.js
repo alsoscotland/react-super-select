eedc5578483a2ab015e9e141bdbf72d34b419547
Scotland Stephenson <scotland.stephenson@oracle.com>
2015-02-09 22:42:24 -0500

    improve jest runner, gitignore compiled jsx in temp add magnifier and icon button styles

--- src/react-super-select.js
+++ src/react-super-select.js
@@ -1,9 +1,42 @@
-var React = require('react');
+var _ = require('../node_modules/lodash'),
+    React = require('react');
 
 var ReactSuperSelect = React.createClass({
 
+  propTypes: {
+    placeholder: React.PropTypes.string,
+    noResultsString: React.PropTypes.string,
+    onChange: React.PropTypes.func,
+    externalMagnifierIconClasses: React.PropTypes.string,
+    searchable: React.PropTypes.bool,
+    remoteDataSourceFetchFunction: React.PropTypes.object,
+    remoteDataSourceIsMultiPage: React.PropTypes.bool,
+    dataSource: React.PropTypes.object,
+    isMultiSelect: React.PropTypes.bool,
+    optionsTemplate: React.PropTypes.object,
+  },
+
+  getInitialState: function() {
+    return {
+      isOpen: false
+    };
+  },
+
+  _getClosedMarkup: function() {
+
+  },
+
+  _getOpenMarkup: function() {
+
+  },
+
   render: function() {
-    return <div>react-super-select</div>;
+    return (
+      <div className="r-ss-wrap">
+        react-super-select
+        <input />
+        <button className="r-ss-button"><i className="r-ss-magnifier">search</i></button>
+    </div>);
   }
 
 });
