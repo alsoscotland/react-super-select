9f1f8bf4b2c399a2cee173e56743cffaaa1c313b
Scotland Stephenson <scotland.stephenson@oracle.com>
2015-02-09 21:13:35 -0500

    watch tasks and jest testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,40 @@
 var gulp = require('gulp'),
-	initGulpTasks = require('react-component-gulp-tasks');
+    jshint = require('gulp-jshint'),
+    shell = require('gulp-shell'),
+    initGulpTasks = require('react-component-gulp-tasks');
 
+
+var paths = {
+  js: ['./src/**/*.js'],
+  lint: {
+    js: [
+      '!./src/**/*.js',
+      './src/**/*-spec.js',
+      'tmp/jsx/**/*.js',
+      'gulpfile.js'
+    ]
+  }
+};
+
+gulp.task('jsx', function() {
+  return gulp.src(['./gulpfile.js'], { read: false })
+    .pipe(shell(['$(npm bin)/jsx ./src/ tmp/jsx --cache-dir tmp/jsx-cache']));
+});
+
+// lint-js-watch mimics lint-js but will not stop watch process on error
+gulp.task('lint-js-watch', ['jsx'], function() {
+  return gulp.src(paths.lint.js)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.js, ['jsx', 'lint-js-watch']);
+});
 
 /**
  * Task configuration is loaded from config.js
- * 
+ *
  * Make any changes to the source or distribution files
  * and directory configuration there
  */
@@ -14,10 +44,10 @@ var config = require('./gulpconfig');
 
 /**
  * Tasks are added by the react-component-gulp-tasks package
- * 
+ *
  * See https://github.com/JedWatson/react-component-gulp-tasks
  * for documentation.
- * 
+ *
  * You can also add your own additional gulp tasks if you like.
  */
 
