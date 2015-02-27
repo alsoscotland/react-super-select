620ef24d6182cf7b6ef51f4ce7dc7bd9a345b718
Scotland Stephenson <scotland.stephenson@oracle.com>
2015-02-27 00:40:45 -0500

    Keyboard based focus navigation adding less linter to gulp devlint process adding custom filter function and search filter function support adding support for custom templating update jest and react styling updates for dropdown overflow and search positioning update react-component-gulp-tasks test data for example files

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 var React = require('react'),
-    ReactSuperSelect = require('react-super-select');
-
+    ReactSuperSelect = require('react-super-select'),
+    ExampleOptionTemplate = require('./example-option-template');
 
 var testData = require('./test-data.js');
 
 var App = React.createClass({
+
+
+  _customMarkupMapper: function(item) {
+    return(
+    <ExampleOptionTemplate option={item} />);
+  },
+
   render: function() {
     return (
       <div>
-        <ReactSuperSelect placeholder="Make a Selection" searchable={true} searchPlaceholder="search" dataSource={testData} />
+        <section className="r-ss-example-section">
+          <h1>Basic Example</h1>
+          <ReactSuperSelect placeholder="Make a Selection" searchable={true} searchPlaceholder="search" dataSource={testData} />
+        </section>
+        <section className="r-ss-example-section">
+          <h1>Custom Template Example</h1>
+          <ReactSuperSelect placeholder="Make a Selection" searchable={true} searchPlaceholder="search" customOptionsMapper={this._customMarkupMapper} dataSource={testData} />
+        </section>
       </div>
     );
   }
