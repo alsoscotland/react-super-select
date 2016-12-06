9bf4c5ab199b55ba75dd24963139f7271a383e49
Scotland Stephenson <scotland.stephenson@oracle.com>
2016-12-05 22:12:03 -0500

    Add controlId prop for isomorphic rendering issue
      - https://github.com/alsoscotland/react-super-select/issues/46

---
+++ src_docs/properties/controlId.js
@@ -0,0 +1,11 @@
+var controlIdMarkdown = require('../markdown/js/properties/controlId').body;
+
+var controlIdProp = {
+
+  nameAttr: "controlId",
+
+  renderString: controlIdMarkdown
+
+};
+
+module.exports = controlIdProp;
