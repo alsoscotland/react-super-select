3e4586dadcd00612918122105b892c52f9f8ec05
Scotland Stephenson <scotland.stephenson@oracle.com>
2017-07-11 10:54:40 -0400

    Issue 115 add an onClear callback

---
+++ src_docs/markdown/properties/onClear.md
@@ -0,0 +1,4 @@
+<!-- onClear -->
+(**Function**)
+
+A callback which will be fired when a user manaually clears the selection(s).
---
+++ src_docs/properties/onClear.js
@@ -0,0 +1,11 @@
+var onClearMarkdown = require('../markdown/js/properties/onClear').body;
+
+var onClearProp = {
+
+  nameAttr: "onClear",
+
+  renderString: onClearMarkdown
+
+};
+
+module.exports = onClearProp;
