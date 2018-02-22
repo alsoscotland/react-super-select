f9f68b8524250850d58bf31054d123f9762c7111
Scotland Stephenson <scotlandrummer@yahoo.com>
2018-02-21 22:30:44 -0500

    Add docs for blur handlers

---
+++ src_docs/properties/onBlur.js
@@ -0,0 +1,11 @@
+var onBlurMarkdown = require('../markdown/js/properties/onBlur').body;
+
+var onBlurProp = {
+
+  nameAttr: "onBlur",
+
+  renderString: onBlurMarkdown
+
+};
+
+module.exports = onBlurProp;
---
+++ src_docs/properties/onSearchInputBlur.js
@@ -0,0 +1,11 @@
+var onSearchInputBlurMarkdown = require('../markdown/js/properties/onSearchInputBlur').body;
+
+var onSearchInputBlurProp = {
+
+  nameAttr: "onSearchInputBlur",
+
+  renderString: onSearchInputBlurMarkdown
+
+};
+
+module.exports = onSearchInputBlurProp;
